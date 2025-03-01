e57cff2b02564f511dc20e28ddbfab404d1380eb
busiP <u24704785@tuks.co.za>
2025-03-01 18:43:03 +0200

    added controller and model

---
+++ busiStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using busiStudyGroup.Models;
+
+
+namespace busiStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult Index()
+        {
+
+            //create student objects (add data to controller)
+            List<StudyGroup> myStudents = new List<StudyGroup>
+            {
+                new StudyGroup
+                {
+                    StudentNum = "u24704785",
+                    Name = "Busiswe",
+                    Surname = "Paul",
+                    Email = "u24704785@tuks.co.za"
+                },
+
+                new StudyGroup
+                {
+                    StudentNum = "u24722490",
+                    Name = "Nyiko",
+                    Surname = "Mbhalati",
+                    Email = "u24722490@tuks.co.za"
+                },
+
+                new StudyGroup
+                {
+                    StudentNum = "u24641342",
+                    Name = "Michael",
+                    Surname = "Neto",
+                    Email = "u24641342@tuks.co.za"
+                },
+
+                new StudyGroup
+                {
+                    StudentNum = "u22675524",
+                    Name = "Thato",
+                    Surname = "Modise",
+                    Email = "u22675524@tuks.co.za"
+                },
+
+                new StudyGroup
+                {
+                    StudentNum = "u24687945",
+                    Name = "Naledi",
+                    Surname = "Nkwana",
+                    Email = "u24687945@tuks.co.za"
+                },
+
+
+
+
+            };
+            return View(myStudents);//link to view
+        }
+    }
+}
---
+++ busiStudyGroup/Models/StudyGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations; //Allow use of decorators
+using System.Linq;
+using System.Web;
+
+namespace busiStudyGroup.Models
+{
+    public class StudyGroup
+    {
+        //add properties from table
+        [DisplayName("Student Number")]
+        [Required(ErrorMessage = "Student number is required")] //Decorator
+        public string StudentNum { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname number is required")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Email number is required")]
+        public string Email { get; set; }
+    }
+}
